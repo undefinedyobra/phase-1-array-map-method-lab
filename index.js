350fd7e52bf9cd0728368792ba2b659e6970d524
undefinedyobra <brianmelly008@gmail.com>
2024-03-25 10:02:27 +0300

    array map method lab

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function toTitleCase(string) {
+  return string.split(' ').map(word => capitalizeFirstLetter(word)).join(' ');
+}
+
+function titleCased() {
+  return tutorials.map(tutorial => toTitleCase(tutorial));
+}
+
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  "what does the this keyword mean?",
+  "What is the Constructor OO pattern?",
+  "implementing Blockchain Web API",
+  "The Test Driven Development Workflow",
+  "What is NaN and how Can we Check for it",
+  "What is the difference between stopPropagation and preventDefault?",
+  "Immutable State and Pure Functions",
+  "what is the difference between == and ===?",
+  "what is the difference between event capturing and bubbling?",
+  "what is JSONP?",
 ];
 
-const titleCased = () => {
-  return tutorials
-}
+module.exports = titleCased;
